7d413fe2ae8daa61768a3ca6512d8cefa051dc7f
future <83049262+future-mi@users.noreply.github.com>
2022-04-21 02:20:49 +0900

    Spring-Boot

--- SpingBootMyBatisProject/src/main/java/com/sist/music/SpingBootMyBatisProjectApplication.java
+++ SpingBootMyBatisProject/src/main/java/com/sist/music/SpingBootMyBatisProjectApplication.java
@@ -2,8 +2,10 @@ package com.sist.music;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.sist.music.dao", "com.sist.music.controller", "com.sist.music.service"})
 public class SpingBootMyBatisProjectApplication {
 
 	public static void main(String[] args) {
---
+++ SpingBootMyBatisProject/src/main/java/com/sist/music/service/MusicService.java
@@ -0,0 +1,12 @@
+package com.sist.music.service;
+
+import java.util.*;
+
+import com.sist.music.vo.MusicVO;
+
+public interface MusicService {
+
+    public List<MusicVO> musicListData(Map map);
+
+    public int musicTotalPage(int cno);
+}
